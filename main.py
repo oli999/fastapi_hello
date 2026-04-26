32ac9985920fe499e429b8fe7ff4258a8db65609
kimgura <oli999@naver.com>
2026-04-26 20:48:34 +0900

    main.py  수정함

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, requests
 
 # 1. FastAPI 인스턴스 생성
 app = FastAPI()
@@ -9,7 +9,30 @@ async def root():
     # 3. JSON 형태로 리턴
     return {"message": "Hello World"}
 
-# 4. 특정 경로 추가 테스트
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}!"}
+# 3. health 체크용
+@app.get("/health")
+async def health():
+    try:
+        # DB ping (예시)
+        # redis ping 등
+        return {"status": "ok"}
+    except:
+        return {"status": "error"}
+
+# 4. EC2 식별용 endpoint
+import socket
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/whoami")
+async def whoami():
+    # 현재 서버의 호스트 이름(컴퓨터 이름) 가져오기
+    hostname = socket.gethostname()
+    
+    # 호스트 이름을 기반으로 사설 IP 주소 알아내기
+    local_ip = socket.gethostbyname(hostname)
+    
+    return {
+        "local_ip": local_ip
+    }
